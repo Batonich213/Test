813cb4ee7dc1d648d0c68d8e773545cba02b606f
Никита Ботов <botov164@gmail.com>
2023-07-04 12:27:26 +0300

    Add project files.

---
+++ Test/Models/TodoContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+namespace Test.Models
+{
+    public class TodoContext : DbContext
+    {
+        public TodoContext(DbContextOptions<TodoContext> options) : base(options) { }
+        public DbSet<Todoitem> TodoItems { get; set; } = null!;
+    }
+}
---
+++ Test/Models/Todoitem.cs
@@ -0,0 +1,9 @@
+﻿namespace Test.Models
+{
+    public class Todoitem
+    {
+        public long Id { get; set; }
+        public string? Name { get; set; }
+        public bool IsComplite { get; set; }
+    }
+}
